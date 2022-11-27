07c1efd3f1614c8de073bdcd4a2f207a93ed0d37
Lee-Roy Ashworth <leeroya@gmail.com>
2022-11-27 15:11:18 +0200

    tests: working on tests

--- tests/Gooi.Tests/MessageHandlerTests.cs
+++ tests/Gooi.Tests/MessageHandlerTests.cs
@@ -1,4 +1,5 @@
 using Moq;
+using Newtonsoft.Json;
 using Skinder.Gooi.Azure;
 using Skinder.Gooi.Contracts.Interfaces.Infrastructure;
 
@@ -6,18 +7,35 @@ namespace Skinder.Gooi.Tests;
 
 public class MessageHandlerTests
 {
+    public MessageHandlerTests()
+    {
+        ConfigureEnvironmentVariablesFromLocalSettings();
+    }
+
     [Fact]
     public async Task MessageHandler_Should_Return0_WithBasicSetup()
     {
         var writer = new Mock<IInfrastructureOutputWriter>();
         
         var messageHandler = new AzureMessageCommandHandler(writer.Object);
-        //
         var result = await messageHandler.Handle(new AzureQueueMessageCommand()
         {
-            Message = "Hello"
+            Message = "Hello",
+            Queue = "demo",
+            ConnectionString = "UseDevelopmentStorage=true"
         }, new CancellationToken());
         
         Assert.True(result == 0);
+    }
+    static void ConfigureEnvironmentVariablesFromLocalSettings()
+    {
+        var path = Path.GetDirectoryName(typeof(MessageHandlerTests).Assembly.Location);
+        var json = File.ReadAllText(Path.Join(path, "local.settings.json"));
+        var parsed = Newtonsoft.Json.Linq.JObject.Parse(json).Value<Newtonsoft.Json.Linq.JObject>("Values");
+
+        foreach (var item in parsed)
+        {
+            Environment.SetEnvironmentVariable(item.Key, item.Value.ToString());
+        }
     }
 }
