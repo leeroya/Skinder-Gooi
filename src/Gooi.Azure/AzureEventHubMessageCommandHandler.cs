af7b4cfae316800dee5487f10f7a7dcc69d4a3b1
Lee-Roy Ashworth <leeroya@gmail.com>
2023-03-05 20:23:26 +0200

    feat: Added Eventhub message sending

---
+++ src/Gooi.Azure/AzureEventHubMessageCommandHandler.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Producer;
+using Skinder.Gooi.Contracts.Interfaces.Infrastructure;
+
+namespace Skinder.Gooi.Azure;
+
+public class AzureEventHubMessageCommandHandler : IRequestHandler<AzureEventHubMessageCommand, int>
+{
+    private readonly IInfrastructureOutputWriter _outputWriter;
+    public AzureEventHubMessageCommandHandler(IInfrastructureOutputWriter outputWriter)
+    {
+        _outputWriter = outputWriter;
+    }
+    public async Task<int> Handle(AzureEventHubMessageCommand request, CancellationToken cancellationToken)
+    {
+        _outputWriter.WriteFaintLine("Azure message handling");
+        EventHubProducerClient client = new EventHubProducerClient(request.ConnectionString , request.Queue);
+        if (string.IsNullOrEmpty(request.Message))
+        {
+            _outputWriter.WriteError("Message was not passed, nothing was sent.");
+            return 1;
+        }
+
+        await InsertMessageAsync(client, request.Message);
+        return 0;
+    }
+    private async Task InsertMessageAsync(EventHubProducerClient theQueue, string message)
+    {
+        // Create a batch of events 
+        using EventDataBatch eventBatch = await theQueue.CreateBatchAsync();
+
+        var encodedMessage = Encoding.UTF8.GetBytes(message);
+        if (!eventBatch.TryAdd(new EventData(encodedMessage)))
+        {
+            throw new Exception($"Event message is too large for the batch and cannot be sent.");
+        }
+        try
+        {
+            await theQueue.SendAsync(eventBatch);
+        }
+        finally
+        {
+            await theQueue.DisposeAsync();
+        }
+    }
+}
--- src/Gooi.Azure/AzureQueueMessageCommand.cs
+++ src/Gooi.Azure/AzureQueueMessageCommand.cs
@@ -4,14 +4,14 @@
 public class AzureQueueMessageCommand: IRequest<int>, IAzureCommandOptions
 {
     [Option("message", SetName = "Azure-Queue", HelpText = "Message to be sent to queue.")]
-    public string Message { get; set; }
+    public string? Message { get; set; }
 
     [Option("expire", SetName = "Azure-Queue", HelpText = "Message to be sent to queue.")]
     public bool Expire { get; set; } = false;
     
     [Option("queue", SetName = "Azure-Queue", HelpText = "Desired queue to send the message.")]
-    public string Queue { get; set; }
+    public string? Queue { get; set; }
 
     [Option("connection", SetName = "Azure-Queue", HelpText = "Azure queue connection string.")]
-    public string ConnectionString { get; set; }
+    public string? ConnectionString { get; set; }
 }
